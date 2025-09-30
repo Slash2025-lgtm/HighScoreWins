4f47642b1d4b06bf4c59a60b5ffea1e854bdceef
Student <bwilliams1@my.yearupunited.org>
2025-09-30 09:22:17 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the score name (Ex Home:Visitor|0:0): ");
+        String Scoreboard = keyboard.nextLine().trim();
+
+        String[] SplitUp = Scoreboard.split("[:, |]");
+        int HomeTeamScore = Integer.parseInt(SplitUp[2]);
+        int AwayTeamScore = Integer.parseInt(SplitUp[3]);
+
+        if (HomeTeamScore > AwayTeamScore) {
+            System.out.println(SplitUp[0] + " Has won");
+        } else {
+            System.out.println(SplitUp[1] + " Has won");
+        }
+    }
+}
